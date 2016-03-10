f97b7eee66cf65cab63e2e224f640cc2e922efb9
Ivan Voronov <ioann.voronov@gmail.com>
2016-03-10 18:32:49 +0200

    Made flower to be hypnotic.

--- BestFlower/js/main.js
+++ BestFlower/js/main.js
@@ -12,6 +12,21 @@ function setColor(collection, color) {
   });
 };
 
+function getNextColor(i, order)
+{
+	var frequency = 0.01;
+    var center = 128;
+    var amplitude = 127;
+	var r = Math.sin(frequency*i - 2*Math.PI*order/8) * amplitude + center;
+	r = parseInt(r);
+	var g = Math.sin(frequency*i + 2*Math.PI/3 - 2*Math.PI*order/8) * amplitude + center;
+	g = parseInt(g);
+	var b = Math.sin(frequency*i + 4*Math.PI/3 - 2*Math.PI*order/8) * amplitude + center;
+	b = parseInt(b);
+	var color = 'rgb(' +  r + ',' + g + ',' + b + ')';
+	return color;
+};
+
 document.addEventListener('DOMContentLoaded', function() {
   var firstLeaf = document.querySelectorAll('.head li:nth-child(1)');
   var secondLeaf = document.querySelectorAll('.head li:nth-child(2)');
@@ -22,14 +37,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var seventhLeaf = document.querySelectorAll('.head li:nth-child(7)');
   var eighthLeaf = document.querySelectorAll('.head li:nth-child(8)');
   
+  var i = 0;
+  
   setInterval(function() {
-    setColor(firstLeaf, getRandomColor());
-	setColor(secondLeaf, getRandomColor());
-	setColor(thirdLeaf, getRandomColor());
-	setColor(fourthLeaf, getRandomColor());
-	setColor(fifthLeaf, getRandomColor());
-	setColor(sixthLeaf, getRandomColor());
-	setColor(seventhLeaf, getRandomColor());
-	setColor(eighthLeaf, getRandomColor());
-  }, 750);
+	i = i + 1;
+    setColor(firstLeaf, getNextColor(i,3));
+	setColor(secondLeaf, getNextColor(i,7));
+	setColor(thirdLeaf, getNextColor(i,6));
+	setColor(fourthLeaf, getNextColor(i,4));
+	setColor(fifthLeaf, getNextColor(i,5));
+	setColor(sixthLeaf, getNextColor(i,2));
+	setColor(seventhLeaf, getNextColor(i,8));
+	setColor(eighthLeaf, getNextColor(i,1));
+  }, 7);
 });
